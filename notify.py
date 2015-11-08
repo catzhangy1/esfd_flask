fb15d2d1bd8d9c429d7dd46591583066fc5ea0c3
hamsterjacks <jackson.chang@berkeley.edu>
2015-11-07 23:00:47 -0800

    Update notify.py so we only connect to server once.

--- notify.py
+++ notify.py
@@ -8,27 +8,43 @@ import db2
 import smtplib
 from email.mime.text import MIMEText
 
-ME = "justin.bieber@jetblue.com"
+API_USER = "easyflightdeals@gmail.com"
+API_KEY = "4IQ28aKA3wuw7bd1zu3j"
 
 
 # Analyzes contacts and sends emails and texts
 def send_email_and_text(db, fields, new_entries):
     persons = db2.field_query(db, fields, "contact_list")
-    mailing_list = dict() # Maps Person -> (Name, ORI->DEST)
+    mailing_list = dict()
     for p in persons:
         if p not in mailing_list:
             mailing_list[(p[1], p[2])] = {
-              "name" : p[0],
-              "matches" : set(),
+                "name": p[0],
+                "matches": set(),
             }
         mailing_list[(p[1], p[2])]["matches"].add("%s -> %s" % (p[3], p[4]))
 
+    server = start_server()
     for k, v in mailing_list.items():
-        send_email(k, v)
+        send_email(k, v, server)
+    quit_server(server)
+
+
+def start_server():
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.ehlo()
+    server.starttls()
+    server.login(API_USER, API_KEY[::-1])
+    return server
+
+
+def quit_server(server):
+    server.quit()
+
 
 # k is tuple of email and phone number
 # v is name and matches
-def send_email(k, v):
+def send_email(k, v, server):
     if k[0]:
         msg = MIMEText(''.join([
             "Hi %s, \n\n" % v["name"],
@@ -43,20 +59,15 @@ def send_email(k, v):
             "Eat Shit Fuck Die",
         ]))
         msg['Subject'] = "[Notification] New Jetblue Getaway Deal!"
-        msg['From'] = ME
+        msg['From'] = API_KEY
         msg['To'] = k[0]
-        username = 'easyflightdeals@gmail.com'
-        password = 'j3uz1db7wuw3AKa82QI4'
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.ehlo()
-        server.starttls()
-        server.login(username,password)
-        server.sendmail(ME, k[0], msg.as_string())
-        server.quit()
+        server.sendmail(API_KEY, k[0], msg.as_string())
 
 
-v = { "name" : "Addison", "matches" : set() }
+v = {"name": "Addison", "matches": set()}
 v["matches"].add("ABC -> DEF")
 v["matches"].add("GFH -> IJK")
 k = ("huisaddison@gmail.com", 0)
-send_email(k, v)
+s = start_server()
+send_email(k, v, s)
+quit_server(s)
